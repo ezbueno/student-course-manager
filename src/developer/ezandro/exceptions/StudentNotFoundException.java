ba15d58ac3d2f85a033cbf79bab3f87f1a634ca8
ezbueno <ezandrobueno@yahoo.com.br>
2025-05-13 23:46:30 -0300

    Add StudentNotFoundException for missing student scenarios

---
+++ src/developer/ezandro/exceptions/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package developer.ezandro.exceptions;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
